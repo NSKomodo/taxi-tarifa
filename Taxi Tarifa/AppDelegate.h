9d33323ca9baf184118542678007a391378aa7f1
Jorge Tapia <jtapia@mindshake.net>
2014-01-30 12:04:32 -0500

    Adds all necessary frameworks.

---
+++ Taxi Tarifa/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Taxi Tarifa
+//
+//  Created by Jorge Tapia on 1/30/14.
+//  Copyright (c) 2014 Mindshake Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Taxi Tarifa/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Taxi Tarifa
+//
+//  Created by Jorge Tapia on 1/30/14.
+//  Copyright (c) 2014 Mindshake Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
