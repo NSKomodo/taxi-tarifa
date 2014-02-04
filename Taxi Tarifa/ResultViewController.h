137d76fec77276f94d6ab65cb22463170f62b952
Jorge Tapia <jtapia@mindshake.net>
2014-02-03 19:12:58 -0500

    Adds complete functionality. v0.9 TODO: switch the night and day calculated fare values.

---
+++ Taxi Tarifa/ResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.h
+//  Taxi Tarifa
+//
+//  Created by Jorge Tapia on 2/3/14.
+//  Copyright (c) 2014 Mindshake Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController : UIViewController
+
+@end
---
+++ Taxi Tarifa/ToViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToViewController.h
+//  Taxi Tarifa
+//
+//  Created by Jorge Tapia on 1/31/14.
+//  Copyright (c) 2014 Mindshake Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToViewController : UIViewController
+
+@end
